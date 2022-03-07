96a75d95e59fb4ab328331b4618bd850ab411b80
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-03-07 14:44:32 +0300

    change the date constuctor method

--- js/index.js
+++ js/index.js
@@ -9,25 +9,23 @@ let button = document.getElementById("btn");
 let form = document.getElementById("birth");
 form.addEventListener("submit",(event) =>{
         event.preventDefault();
+        
         let formData = new FormData(event.target)
-        var year = document.getElementById("year").value;
+        // let myDate = new Date(formData.get("date"));
+        let year =document.getElementById("year").value;
         var month = document.getElementById("month").value;
-        var myDate = document.getElementById("date").value;
-        var gender = document.getElementsByName("gender");
-     
-    // console.log(typeof year)
+        var date = document.getElementById("date").value;
+        let myDate = new Date(year,--month,date);
+        let dayOfWeek = myDate.getDay();
+         alert(dayOfWeek)
+       
+          let gender = document.getElementsByName("gender");
 
-        for (var i = 0; i < gender.length; i++) {
+           for (var i = 0; i < gender.length; i++) {
             if (gender[i].checked)
                 var genderValue = gender[i].value;
         }
-        let cen = Number(year.slice(0,2));
-        let yy = Number(year.slice(2));
-        // console.log(yy)
-        let mm =Number(month)
-        let dd =Number(myDate)
-        let calculateDayOfWeek  = ( ( (cen/4) -2*cen-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
-        let dayOfWeek = calculateDayOfWeek.toFixed() -1;
+       
     
         if (genderValue == "Female"){
             document.getElementById("output").innerHTML = ("You were born on" + " "  + day[dayOfWeek]+ " " +"so your Akan name is"+ " " +  female[dayOfWeek] );
@@ -38,11 +36,25 @@ form.addEventListener("submit",(event) =>{
         }
     
     });
+     function output(){
+     document.getElementById("output").innerHTML =""
+
+    }
+
+
+    // PREVIOUS CODE
+ // var year = document.getElementById("year").value;
+        // var month = document.getElementById("month").value;
+        // var myDate = document.getElementById("date").value;
+        // var gender = document.getElementsByName("gender");
+     
+    // console.log(typeof year)
+
+ // let cen = Number(year.slice(0,2));
+        // let yy = Number(year.slice(2));
+        // console.log(yy)
+        // let mm =Number(month)
+        // let dd =Number(myDate)
+        // let calculateDayOfWeek  = ( ( (cen/4) -2*cen-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+        // let dayOfWeek = calculateDayOfWeek.toFixed() -1;
        
-
-
-
-
-
-
-
